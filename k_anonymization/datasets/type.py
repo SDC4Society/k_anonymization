ad53e38f032fc10c5caabda1fc27ba6ed297db84
Chanh <chanh@ism.ac.jp>
2025-11-14 16:36:51 +0900

    feat: taxonomy tree display also for numerical attributes

--- k_anonymization/datasets/type.py
+++ k_anonymization/datasets/type.py
@@ -20,7 +20,7 @@ class HierarchiesDict(dict):
         num_of_attributes: int,
         qids: list,
         qids_idx: list,
-        is_categorical: list,
+        first_row: list,
     ):
         self.hierarchies_dir = hierarchies_dir
         self.__qids = []
@@ -28,7 +28,7 @@ class HierarchiesDict(dict):
             self.__qids.extend([None] * (qids_idx[pos] - len(self.__qids)))
             self.__qids.append(qid)
         self.__qids.extend([None] * (num_of_attributes - len(self.__qids)))
-        self.cat_qids = [qids[i] for i, v in enumerate(is_categorical) if v is True]
+        self.first_row = first_row
 
     def __getitem__(self, key):
         if isinstance(key, int):
@@ -39,42 +39,61 @@ class HierarchiesDict(dict):
                 )
         else:
             _key = key
+            if _key is None or _key not in self.__qids:
+                raise AttributeError(f"Cannot find attribute '{_key}'")
         if _key not in self.keys():
             try:
                 with open(f"{self.hierarchies_dir}/{_key}.json") as f:
-                    super().__setitem__(_key, HierarchyDict(json.load(f)))
+                    super().__setitem__(
+                        _key,
+                        HierarchyDict(
+                            self.first_row[self.__qids.index(_key)],
+                            json.load(f),
+                        ),
+                    )
             except:
-                raise FileNotFoundError(f'Cannot find the hierarchy "{_key}".')
+                raise FileNotFoundError(f'Cannot find hierarchy file of "{_key}".')
         return super().__getitem__(_key)
 
-    def display_all_trees(self):
-        if self.cat_qids == []:
-            return "No Categorical Attribute"
+    @cached_property
+    def generalization_trees(self):
+        qids = [x for x in self.__qids if x is not None]
         children = [
             Image(
-                value=self.__getitem__(cat_qid).display_tree().pipe(format="png"),
+                value=self.__getitem__(qid).generalization_tree.pipe(format="png"),
                 format="png",
                 layout=Layout(max_width="unset"),
             )
-            for cat_qid in self.cat_qids
+            for qid in qids
         ]
         stack = Stack(children, selected_index=0)
-        dropdown = Dropdown(options=self.cat_qids)
+        dropdown = Dropdown(options=qids)
         jslink((dropdown, "index"), (stack, "selected_index"))
-        return VBox([Label("Categorical Taxonomy Tree:"), dropdown, stack])
+        return VBox([Label("Generalization Trees:"), dropdown, stack])
 
 
 class HierarchyDict(dict):
-    def __init__(self, *args, **kwargs):
+    def __init__(
+        self,
+        first_row,
+        *args,
+        **kwargs,
+    ):
         super().__init__(*args, **kwargs)
+        self.first_row = first_row
         if "tree" in self.keys():
             self.height = len(self.__getitem__("tree"))
-            self.is_categorical = True
-        else:
-            self.is_categorical = False
 
-    def display_tree(self):   
-        assert "tree" in self.keys(), 'Property "tree" not provided'
+    @cached_property
+    def generalization_tree(self):
+        if "tree" in self.keys():
+            return self.__get_generalization_tree_from_tree()
+        elif "lambda" in self.keys():
+            return self.__get_generalization_tree_from_lambda()
+        else:
+            return "No generalization provided/found."
+
+    def __get_generalization_tree_from_tree(self):
         tree = Digraph(
             edge_attr={"arrowhead": "none"},
             node_attr={"shape": "plaintext"},
@@ -104,6 +123,19 @@ class HierarchyDict(dict):
                         for values in node["values"]
                     ]
                 )
+        return tree
+
+    def __get_generalization_tree_from_lambda(self):
+        tree = Digraph(graph_attr={"rankdir": "LR"})
+        lambdas = self.__getitem__("lambda")
+        nodes = [self.first_row]
+        for f in lambdas:
+            nodes.append(eval(f)(nodes[-1]))
+        for i, v in enumerate(nodes[0:-1]):
+            if i == 0:
+                tree.edge(str(nodes[0]), str(nodes[1]), label='(1)')
+            else:
+                tree.edge(str(nodes[i]), str(nodes[i+1]), label=f"({i+1})")
         return tree
 
 
@@ -190,7 +222,7 @@ class Dataset:
                 len(list(self.df)),
                 self.qids,
                 self.qids_idx,
-                self.is_categorical,
+                self.df[0:1].values[0].tolist(),
             )
         return self.__hierarchies
 
