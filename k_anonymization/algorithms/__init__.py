476008a88e8544ecc115f91739947019723d9765
Chanh <chanh@ism.ac.jp>
2025-11-28 11:13:05 +0900

    feat: perturbation algorithm

--- k_anonymization/algorithms/__init__.py
+++ k_anonymization/algorithms/__init__.py
@@ -1,5 +1,6 @@
 from .clustering_based import KMember, OKA
 from .datafly.datafly import Datafly
+from .probabilistic import Perturbation
 from .utils import *
 
-__all__ = ["Datafly", "KMember", "OKA"]
+__all__ = ["Datafly", "KMember", "OKA", "Perturbation"]
