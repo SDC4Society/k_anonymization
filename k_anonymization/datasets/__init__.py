ba6cd8212dd2469909d373c5776cfea421bc1846
Chanh <chanh@ism.ac.jp>
2026-02-12 20:52:44 +0900

    feat: london_house_price dataset

--- k_anonymization/datasets/__init__.py
+++ k_anonymization/datasets/__init__.py
@@ -5,6 +5,7 @@ from .type import Dataset, DataFrameTable, SampleDataset
 
 
 ADULT = Dataset("adult")
+LONDON_HOUSE_PRICE = Dataset("london_house_price")
 MINI_CRIME = Dataset("mini_crime")
 MINI_PATIENT = Dataset("mini_patient")
 
